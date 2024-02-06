9196e8144509739b6fa139b0b4dea97bcdb44a06
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-02-06 11:35:16 +0530

    DLL recent changes 3

--- src/main/java/com/dsa/algorithm/dll/DoublyLinkedList.java
+++ src/main/java/com/dsa/algorithm/dll/DoublyLinkedList.java
@@ -146,26 +146,137 @@ public class DoublyLinkedList {
 	}
 
 	public boolean insert(int index, int value) {
-		if (index < 0 || index > length ) {
+		if (index < 0 || index > length) {
 			return false;
 		}
-		if(index == 0 ) {
+		if (index == 0) {
 			prepend(value);
 			return true;
 		}
-		if(index == length) {
+		if (index == length) {
 			append(value);
 			return true;
 		}
-		
-		Node current = get(index-1);
+
+		Node current = get(index - 1);
 		Node newNode = new Node(value);
 		newNode.next = current.next;
 		current.next.prev = newNode;
-		newNode.prev =current;
+		newNode.prev = current;
 		current.next = newNode;
 		length++;
 		return true;
+	}
+
+	public Node remove(int index) {
+		if (index < 0 || index > length - 1) {
+			return null;
+		}
+		if (index == 0) {
+			return removeFirst();
+		}
+		if (index == length - 1) {
+			return removeLast();
+		}
+
+		Node current = get(index);
+		current.prev.next = current.next;
+		current.next.prev = current.prev;
+		current.next = null;
+		current.prev = null;
+		length--;
+		return current;
+	}
+
+	public void swapFirstLast() {
+		if (length > 1) {
+			int temp = head.value;
+			head.value = tail.value;
+			tail.value = temp;
+		}
+
+	}
+
+	public void reverse() {
+
+		if (head != null) {
+			Node current = head;
+			Node prev = null;
+			Node after = current.next;
+
+			while (current != null) {
+				current.next = prev;
+				current.prev = after;
+				prev = current;
+				current = after;
+
+				if (after != null) {
+					after = after.next;
+				}
+			}
+
+			Node temp = head;
+			head = tail;
+			tail = temp;
+
+		}
+
+	}
+
+	public boolean isPalindrome() {
+		int i = 0;
+		int j = length - 1;
+		Node start = head;
+		Node end = tail;
+		boolean isPalin = true;
+		if (length <= 1)
+			return true;
+
+		while (j >= i) {
+			if (start.value != end.value) {
+				isPalin = false;
+				break;
+			}
+			start = start.next;
+			end = end.prev;
+			i++;
+			j--;
+		}
+		return isPalin;
+	}
+
+	public void swapPairs() {
+        
+		if (length > 1) {
+		    System.out.println("length: "+length);
+			Node curr = head;
+			Node prev = null;
+			Node after = curr.next;
+
+			while (curr != null && after != null) {
+			    
+				curr.next = after.next;
+				System.out.println("Current: " + curr.value);
+			    System.out.println("after: " + after.value);
+				if (after.next != null)
+					after.next.prev = curr;
+				after.prev = prev;
+				after.next = curr;
+				if (prev != null)
+					prev.next = after;
+				curr.prev = after;
+				if(curr == head)
+				    head = after;
+				prev = curr;
+				
+				curr = curr.next;
+				if(curr != null)
+				    after = curr.next;
+
+			}
+			System.out.println("Finihed: " + length);
+		}
+
 	}
 
 }
