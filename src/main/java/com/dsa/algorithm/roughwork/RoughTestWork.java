a930aab46c66d91a11648d59ad2865a70988bff1
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-10-15 23:30:11 +0530

    latest changes

--- src/main/java/com/dsa/algorithm/roughwork/RoughTestWork.java
+++ src/main/java/com/dsa/algorithm/roughwork/RoughTestWork.java
@@ -2,8 +2,18 @@ package com.dsa.algorithm.roughwork;
 
 class A {
 	
+	private static int a= 10;
+	
 	static class B {
 		// inner classes can be static
+	}
+	
+	class C {
+		
+		void print() {
+			System.out.println("a:" + a); //inner classes can acces private variables
+		}
+		
 	}
 }
 
