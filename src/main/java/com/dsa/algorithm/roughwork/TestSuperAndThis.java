1e2c074527839b93a678321e78edd70b2c450010
Arun Gandhi <sivaji.arun2007@gmail.com>
2026-01-09 14:55:56 +0530

    test change

---
+++ src/main/java/com/dsa/algorithm/roughwork/TestSuperAndThis.java
@@ -0,0 +1,21 @@
+package com.dsa.algorithm.roughwork;
+
+
+class ATest {
+	
+	int a;
+	
+	public ATest() {
+		System.out.println("Hi");
+		this.a = 10;
+	}
+	
+}
+
+public class TestSuperAndThis {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
